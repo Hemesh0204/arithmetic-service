cd42617277da4b6a1a06a8b9b1ba7ad4e46dc072
Nisarg Shah <nisarg0606@gmail.com>
2024-02-09 22:27:58 -0500

    updates api development

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Arithmetic service - Hello World!');
 }); 
-app.get('/add/:n/:m', (req, res) => {
-    res.json(Number(req.params.n) + Number(req.params.m));
-  }); 
+
+app.get('/add/:expression', (req, res) => {
+  // Extracting expression from URL parameters
+  const expression = req.params.expression;
+
+  // Evaluating the expression
+  let result;
+  try {
+      result = eval(expression);
+      res.send(${result});
+  } catch (error) {
+      res.status(400).send('Invalid expression');
+  }
+}); 
  
 app.listen(port);
